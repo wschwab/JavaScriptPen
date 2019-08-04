bca35a603c5a3ad7a2188e56e0ccd00583916ffa
wschwab <w.s.schwab4@gmail.com>
2019-08-04 11:30:51 +0300

    fixed checkboxes

--- react_todo/src/components/TodoItem.jsx
+++ react_todo/src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import "../styles/TodoItem.css"
 
 const TodoItem = (props) => (
   <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input type="checkbox" onChange={() => props.handleChange(props.item.id)} checked={props.item.completed} />
       <p>{props.item.text}</p>
   </div>
 )
