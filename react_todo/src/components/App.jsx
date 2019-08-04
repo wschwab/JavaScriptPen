3eb0b244574ad7d8410de286b018c1c9dd832409
wschwab <w.s.schwab4@gmail.com>
2019-08-04 11:10:23 +0300

    changed App into class

--- react_todo/src/components/App.jsx
+++ react_todo/src/components/App.jsx
@@ -4,14 +4,23 @@ import TodoItem from "./TodoItem"
 import '../todoData'
 import '../styles/App.css'
 
-function App() {
-  const todoItems = todoData.map(item => <TodoItem key={item.id} item={item} />)
+class App() extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData
+    }
+  }
 
-    (
+  render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+
+    return (
       <div className="todo-list">
           {todoItems}
       </div>
     )
+  }
 }
 
 export default App
